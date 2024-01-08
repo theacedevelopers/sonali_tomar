7d46d68955e77f4219eb2f5219f0414fde45f027
The Ace Developers <151608560+theacedevelopers@users.noreply.github.com>
2024-01-08 06:05:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,22 +15,3 @@ const sr = ScrollReveal({
 sr.reveal('.logo',{delay:200, origin:'left'});
 sr.reveal('.navbar',{delay:400, origin:'top'});
 sr.reveal('.menu-btn',{delay:500, origin:'right'});
-
-
-let options = {
-  startAngle: -1.55,
-  size: 150,
-  value: 0.85,
-  fill: {gradient: ['#a445b2', '#fa4299']}
-}
-$(".circle .bar").circleProgress(options).on('circle-animation-progress',
-function(event, progress, stepValue){
-  $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
-});
-$(".js .bar").circleProgress({
-  value: 0.70
-});
-$(".react .bar").circleProgress({
-  value: 0.60
-});
-
